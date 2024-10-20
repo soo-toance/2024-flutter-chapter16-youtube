ce54d6ee7f9a7fe6c4804d74bf7db9bacef46275
may <soo.toance@gmail.com>
2024-10-20 20:48:58 +0900

    동영상 홈 화면에 보여주기

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:youtube/component/custom_youtube_player.dart';
+
+import '../model/video_model.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -6,7 +9,13 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+      backgroundColor: Colors.black,
+      body: CustomYoutubePlayer(
+        videoModel: VideoModel(
+          id: '3Ck42C2ZCb8',
+          title: '다트 언어 기본기 1시간만에 끝내기'
+        ),
+      )
     );
   }
 }
